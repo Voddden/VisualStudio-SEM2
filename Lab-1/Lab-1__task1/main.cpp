1359e69f3d697475a1f7563334b1b328f69a827e
Danik <voddden@gmail.com>
2023-02-20 23:10:59 +0300

    отказ от вижловских безопасных функций

--- Lab-1/Lab-1__task1/main.cpp
+++ Lab-1/Lab-1__task1/main.cpp
@@ -1,31 +1,23 @@
-﻿#include "tasks.h"
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "tasks.h"
 
 int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "ru");
     // создание файла
-    errno_t err;
-    FILE* file;
-    err = fopen_s(&file, argv[1], "w");
+    
+    //FILE* file;
+    //errno_t err = fopen_s(&file, argv[1], "w");
+    FILE* file = fopen(argv[1], "w");
     if (file == NULL) {
         printf("Error\n");
         return 1;
     }
 
-    // задача1: ввод чисел в файл
-    task1(file);
-    // задача2: вывод суммы четных чисел
-    task2(file);
-    // задача3: Удалить все чётные числа.
-    task3(file);
-    // задача4: Отсортировать только числа большие заданного по убыванию
-    task4(file);
-
-    // вывод файла в консоль
-    fclose(file);
-    err = fopen_s(&file, argv[1], "r");
-
-    printf("Чтение из файла: ");
+    //// вывод изначального файла в консоль:
+    //err = freopen_s(&file, argv[1], "r", file);
+    printf("Файл по умолчанию: ");
     char c;
     while ((c = getc(file)) != EOF)
     {
@@ -33,6 +25,27 @@ int main(int argc, char* argv[])
     }
     printf("\n");
 
-    // закрытие файла
+    //// работа с файлом:
+   // err = freopen_s(&file, argv[1], "w", file);
+    // задача1: С клавиатуры заполнить файл целыми числами
+    //task1(file);
+    // задача2: Подсчитать сумму чётных чисел
+    //task2(file);
+    // задача3: Удалить все чётные числа
+    //task3(file);
+    // задача4: Отсортировать только числа большие заданного (по убыванию)
+    //task4(file);
+    ////
+    
+    // вывод преобразованного файла в консоль:
+    //err = freopen_s(&file, argv[1], "r", file);
+    printf("Файл после преобразования: ");
+    char ch;
+    while ((ch = getc(file)) != EOF)
+    {
+        printf("%c", ch);
+    }
+    printf("\n");
+
     fclose(file);
 }
