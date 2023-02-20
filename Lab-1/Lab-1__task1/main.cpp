257ede41ee92dbc4c06aa9ae88a526e9a85388dc
Danik <voddden@gmail.com>
2023-02-21 01:15:04 +0300

    nii_agroprom3

--- Lab-1/Lab-1__task1/main.cpp
+++ Lab-1/Lab-1__task1/main.cpp
@@ -8,46 +8,14 @@ void check(FILE* fp) {
     }
 }
 
-void write(FILE* fp, const char* str) {
-    int length = 100;
-    char* message = (char*)malloc(length);
-    //printf("Enter integer numbers: ");
-    //gets_s(message, length);
-    strcpy(message, "12345");
-    if (hasLetters(message)) {
-        printf("Error! Letters are not allowed\n");
-        exit(0);
-    }
-    fflush(stdin);
-
-    for (int i = 0; message[i] != '\0'; i++)
-    {
-        putc(message[i], fp);
-    }
-
-    printf("\nFile has been written\n\n");
-}
-
-
 int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "ru");
     // создание файла
-    
     FILE* file = fopen(argv[1], "r");
     check(file);
 
-    //char* Default = (char*)malloc(100);
-    //strcpy(Default, "13 202 912 11");
-    //fflush(stdin);
-    //for (int i = 0; Default[i] != '\0'; i++)
-    //{
-    //    putc(Default[i], file);
-    //}
-    //write(file, "12345");
-
-    //// вывод изначального файла в консоль:
-    //file = freopen(argv[1], "r", file);
+    //// вывод изначального файла в консоль
     printf("Файл по умолчанию: ");
     char c;
     while ((c = getc(file)) != EOF)
@@ -57,11 +25,11 @@ int main(int argc, char* argv[])
     printf("\n");
 
     //// работа с файлом:
-    //file = freopen(argv[1], "w", file);
+    file = freopen(argv[1], "w", file);
     // задача1: С клавиатуры заполнить файл целыми числами
-    //task1(file);
+    task1(file);
     // задача2: Подсчитать сумму чётных чисел
-    //task2(file);
+    task2(file);
     // задача3: Удалить все чётные числа
     //task3(file);
     // задача4: Отсортировать только числа большие заданного (по убыванию)
@@ -69,7 +37,7 @@ int main(int argc, char* argv[])
     ////
     
     // вывод преобразованного файла в консоль:
-    //file = freopen(argv[1], "r", file);
+    file = freopen(argv[1], "r", file);
     printf("Файл после вызова функций: ");
     char ch;
     while ((ch = getc(file)) != EOF)
--- Lab-1/Lab-1__task1/task2.cpp
+++ Lab-1/Lab-1__task1/task2.cpp
@@ -1,17 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "tasks.h"
 
 /*
 2. Посчитать сумму чётных чисел.
 */
 
-void task2(FILE* fp) {
-	const int length = 10;
-	char* str[length];
-	if (fread(str, sizeof(int), length, fp) != length) {
-		if (feof(fp)) 
-			printf("Premature end of file.");
-		else 
-			printf("File read error.");
+int numberCount(FILE* input) {
+	fseek(input, 0, SEEK_SET);
+	int counter = 0;
+	while (true) {
+		int value;
+		if (fscanf(input, "%d", &value) == 1)
+			counter++;
+		if (feof(input))
+			break;
 	}
+	return counter;
+}
+
+void read_numbers(FILE* input, int size, int* numbers) {
+	fseek(input, 0, SEEK_SET);
+	for (int i = 0; i < size; ++i) {
+		fscanf(input, "%d", &numbers[i]);
+	}
+}
+
+void task2(FILE* fp) {
+
+	int length = numberCount(fp);
+	int* arr = (int*)malloc(length*sizeof(int));
+	read_numbers(fp, length, arr);
 
 }
