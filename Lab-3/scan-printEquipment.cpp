8fc082797f9d7273003d99939c44673d7118568e
Danik <voddden@gmail.com>
2023-03-26 18:37:12 +0300

    delete function scanList()

--- Lab-3/scan-printEquipment.cpp
+++ Lab-3/scan-printEquipment.cpp
@@ -50,22 +50,6 @@ void inputGeneral(Equipment* list, int& size) {
 	}
 }
 
-void scanList(Equipment* stack, int& size) {
-	for (int i = 0; i < size; ++i) {
-		printf("list[%d].id = ", i); stack[i].id = inputInteger("1");
-		printf("list[%d].name = ", i); scanf("%s", &stack[i].name);
-		printf("list[%d].type = ", i); scanf("%s", &stack[i].type);
-		printf("list[%d].quantity = ", i); stack[i].quantity = inputNaturalAnd0("1");
-
-		printf("list[%d].date = ", i); 
-		scanf("%s", &stack[i].date);
-		while(invalidDate(stack[i].date)) {
-			printf("Error. Try again\n");
-			scanf("%s", &stack[i].date);
-		}
-	}
-}
-
 void printEquipment(Equipment equipment) {
 	printf("id = %ld\nname = %s\ntype = %s\nquantity = %d\ndate = %s\n",
 		equipment.id, equipment.name, equipment.type, equipment.quantity, equipment.date);
